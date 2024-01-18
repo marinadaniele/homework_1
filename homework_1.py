cb322ef100f37e3c0c09d8828ae5975a9c47bee0
marinadaniele <marinadanielee@gmail.com>
2024-01-18 16:09:47 +0100

    first homework

--- homework_1.py
+++ homework_1.py
@@ -1,3 +1,38 @@
 print("hello world, this is my first homework")
 
-print("hello world, this is added content to my first homework!!")
+print("hello world, this is added content to my first homework!!!")
+
+print(5+2-9)
+print(2+3*5)
+print(--5)
+print(4/2)
+print(3.0*1.0)
+print(2**3+1)
+print(2<3<1)
+print(3>(0**0*4))
+print(2>1)
+
+print(abs(-38))
+print(74938472//23974)
+print(329735827%92378423)
+print(int(35/5))
+
+print(32>41)
+print(61|34)
+
+speed = 35.0
+time = 20.0
+distance = speed * time
+print(distance)
+
+a = 6
+print(a + 2)
+a /= 2
+print(a)
+b = 3
+print(a ** b)
+b -= a
+print(b+2)
+c = a > b
+print( c and a)
+print( a+b+c )
